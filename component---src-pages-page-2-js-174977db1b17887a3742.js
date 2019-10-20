2460548c513868c42ccb73775c22127c2175a2e7
Gabriel Pérez <gabriel@garox.org>
2019-10-20 18:23:38 -0300

    Updates

---
+++ component---src-pages-page-2-js-174977db1b17887a3742.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{641:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(136),c=a(639),r=a(638);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-174977db1b17887a3742.js.map
