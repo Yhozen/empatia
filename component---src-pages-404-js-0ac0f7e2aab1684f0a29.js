bceb2670f08553f0a8d68b1a9fb5d76b11320a70
Gabriel Pérez <gabriel@garox.org>
2019-10-22 01:26:02 -0300

    Updates

---
+++ component---src-pages-404-js-0ac0f7e2aab1684f0a29.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{662:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(660),o=n(658);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0ac0f7e2aab1684f0a29.js.map
