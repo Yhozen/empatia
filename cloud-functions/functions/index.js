c74a1b6ff75d1b634b3b3e2eafb814531e81620e
Gabriel Pérez <gabriel@garox.org>
2019-10-22 19:22:05 -0300

    fix createdAt

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -4,6 +4,38 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+exports.postsCreatedAt = functions.firestore
+  .document("posts/{postId}")
+  .onCreate((snap, context) => {
+    return snap.ref
+      .set(
+        {
+          createdAt: admin.firestore.FieldValue.serverTimestamp()
+        },
+        { merge: true }
+      )
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
+  });
+
+exports.offeredCreatedAt = functions.firestore
+  .document("offered/{offeredId}")
+  .onCreate((snap, context) => {
+    return snap.ref
+      .set(
+        {
+          createdAt: admin.firestore.FieldValue.serverTimestamp()
+        },
+        { merge: true }
+      )
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
+  });
+
 exports.neededCreatedAt = functions.firestore
   .document("needed/{neededId}")
   .onCreate((snap, context) => {
