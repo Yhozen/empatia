05e85574f794fc0090ff63ab97014ece707ea21d
Gabriel Pérez <gabriel@garox.org>
2019-10-22 15:26:01 -0300

    Updates

---
+++ component---src-pages-needed-categories-js-6bebc0e3b8514a313e42.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{678:function(n,e,t){"use strict";t.r(e);var a=t(0),c=t.n(a),u=t(107);e.default=function(){return c.a.useEffect((function(){Object(u.navigate)("/")}),[]),c.a.createElement("div",null)}}}]);
+//# sourceMappingURL=component---src-pages-needed-categories-js-6bebc0e3b8514a313e42.js.map
