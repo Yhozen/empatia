79655197404244f7a8182481f7776dabc8893ccd
Gabriel Pérez <gabriel@garox.org>
2019-10-30 18:59:22 -0300

    cloud

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,8 +1,10 @@
 const functions = require("firebase-functions");
-
+const { api } = require("./graphql-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+
+exports.api = api;
 
 exports.postsCreatedAt = functions.firestore
   .document("posts/{postId}")
